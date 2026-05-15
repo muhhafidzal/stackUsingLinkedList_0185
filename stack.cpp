1d8be56053e9a778d52b5a8fcf1f651d7d6784c8
muhhafidzal <muh.hafidz.al.mubaroq@gmai.com>
2026-05-15 14:39:42 +0700

    menambahkan implementasi push pada stack

--- stack.cpp
+++ stack.cpp
@@ -27,4 +27,15 @@ public:
         top = NULL; // Initialize the stack with a null pointer
     }
 
+    // Push operation: Insert an element onto the top of the stack
+    int push(int value)
+    {
+        Node *newNode = new Node(); // 1. Allocate memory for the new node
+        newNode->data = value;      // 2. assign value
+        newNode->next = top;        // 3. Set the next pointer of the new node
+        top = newNode;              // 4. update the top pointer to the new node
+        cout << "Push value: " << value << endl;
+        return value;
+    }
+    
 }
