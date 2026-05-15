b3078d3d92e8d35007031e068635d8c3d09d38d8
muhhafidzal <muh.hafidz.al.mubaroq@gmai.com>
2026-05-15 14:51:41 +0700

    menambahkan implementasi peek pada stack

--- stack.cpp
+++ stack.cpp
@@ -56,6 +56,19 @@ public:
     //peek/pop operation.
     void peek()
     {
-
+         if (isEmpty())
+        {
+            cout << " List is Empty." << endl;
+        }
+        else
+        {
+            Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        } // return the value of the top node
     }
 }
