002441de9cf00bab61079093322d7842adc50d60
muhhafidzal <muh.hafidz.al.mubaroq@gmai.com>
2026-05-15 14:29:51 +0700

    menambahkan implementasi node linked list

--- stack.cpp
+++ stack.cpp
@@ -4,3 +4,13 @@ using namespace std;
 
 // Node class representing a single node in the Linked List
 class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node()
+    {
+        next = NULL;
+    }
+};
